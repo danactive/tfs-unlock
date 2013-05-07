0c9a98a8b94054139e4d20811bc1ab8749f4d518
Dan BROOKS <githubcom@domaindesign.ca>
2013-05-06 17:40:21 -0700

    Static one file TFS checkout

--- app/execute-commands.js
+++ app/execute-commands.js
@@ -3,7 +3,9 @@ var exec = require('child_process').exec;
 exports.exe = function (cmd) {
 	if( cmd ) {
 		exec(cmd, function (error, interpreterOutput, interpreterError) {
-			console.log('{"interpreterOutput":' + interpreterOutput + ',"interpreterError":"' + interpreterError + '","cmd":"' + cmd + '"}\n');
+			console.log('\n\nInterpreter Output:\n' + interpreterOutput);
+			console.log('\n\nInterpreter Error:\n' + interpreterError);
+			console.log('\n\ncmd:\n' + cmd);
 		});
 	} else {
 		console.log('Missing cmd.exe query string variable\n');
--- init.bat
+++ init.bat
@@ -1,1 +1,1 @@
-cmd.exe /k "npm install -g grunt-cli testem@0.2.83"
+cmd.exe /k "npm install -g grunt-cli"
