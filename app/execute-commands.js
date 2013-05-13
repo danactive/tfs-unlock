ca728c05b8e5aa6ced1db5ba9dd73e3dce4bfeb3
Dan BROOKS <githubcom@domaindesign.ca>
2013-05-12 21:44:07 -0700

    Remove excessive logging

--- app/execute-commands.js
+++ app/execute-commands.js
@@ -1,15 +1,12 @@
-var exec = require('child_process').exec;
-
 exports.exe = function (cmd) {
-	if( cmd ) {
-		exec(cmd, function (error, interpreterOutput, interpreterError) {
+	var shell = require('child_process').exec;
+	if (cmd) {
+		shell(cmd, function (error, interpreterOutput, interpreterError) {
 			console.log('\n\nInterpreter Output:\n' + interpreterOutput);
 			console.log('\n\nInterpreter Error:\n' + interpreterError);
 			console.log('\n\ncmd:\n' + cmd);
 		});
 	} else {
-		console.log('Missing cmd.exe query string variable\n');
+		console.log('Missing shell command\n');
 	}
 };
-
-console.log('command.js');
--- app/runner.js
+++ app/runner.js
@@ -1,5 +1,4 @@
-var cmd = require('./execute-commands.js');
-
-cmd.exe("cd C:/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/IDE/ & tf.exe checkout C:/web/RDC/Realtor/MVC.realtor.com/Maintenance/Move.Realtor.Web/App_Data/Environment/QA.Environment.config");
-
-console.log('runner.js')
+exports.tfsCheckout = function (cmd) {
+	var cmd = require('./execute-commands.js');
+	cmd.exe("cd C:/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/IDE/ & tf.exe checkout C:/web/RDC/Realtor/MVC.realtor.com/Maintenance/Move.Realtor.Web/App_Data/Environment/QA.Environment.config");
+};
