a79101083a752feb25322ea359f3b7e0352b6718
Dan BROOKS <githubcom@domaindesign.ca>
2013-05-18 12:53:52 -0700

    Move path to CWD arg

--- tfs-unlock.js
+++ tfs-unlock.js
@@ -2,7 +2,7 @@ var shell = {
 	"exe": function (command) {
 		var process = require('child_process').exec;
 		if (command) {
-			process(command, function (error, interpreterOutput, interpreterError) {
+			process(command, { "cwd": 'C:/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/IDE/' }, function (error, interpreterOutput, interpreterError) {
 				console.log('\n\nInterpreter Output:\n' + interpreterOutput);
 				console.log('\n\nInterpreter Error:\n' + interpreterError);
 				console.log('\n\nCommand:\n' + command);
@@ -15,7 +15,7 @@ var shell = {
 
 exports.checkout = function (verifiedPaths) { // verfied meaning the path and file exisit
 	verifiedPaths.forEach(function (filepath) {
-		var command = "cd C:/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/IDE/ & tf.exe checkout " + filepath;
+		var command = "tf.exe checkout " + filepath;
 		shell.exe(command);
 	});
 };
