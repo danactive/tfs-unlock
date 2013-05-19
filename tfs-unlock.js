62b460bdae29db22661461b50de78592e830f48b
Dan BROOKS <githubcom@domaindesign.ca>
2013-05-19 13:19:37 -0700

    Wrapper defines path to Visual Studio

--- tfs-unlock.js
+++ tfs-unlock.js
@@ -1,18 +1,22 @@
-var shell = {
+var callback,
+shell = {
 	"exe": function (command) {
 		var process = require('child_process').exec,
 			child;
 		if (command) {
-			child = process(command, { "cwd": 'C:/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/IDE/' });
+			child = process(command, { "cwd": workingDirectory });
 
 			child.stdout.on('data', function (data) {
-				console.log('Interpreter Output: ' + data);
+				console.log('Interpreter Output: ' + data + '.');
 			});
 			child.stderr.on('data', function (data) {
-				console.log('Interpreter Error: ' + data);
+				console.log('Interpreter Error: ' + data + '.');
 			});
-			child.on('close', function (code) {
-				console.log('Child process exited with code ' + code);
+			child.on('close', function (code, signal) {
+				console.log('Child process exited with code # ' + code + '.');
+				if (signal !== null) {
+					console.log('child process terminated due to receipt of signal ' + signal + '.');
+				}
 			});
 
 			return "Begin shell command";
@@ -21,20 +25,42 @@ var shell = {
 		}
 	}
 },
-tfs = function (verifiedPaths, command, callback) { // verfied meaning the path and file exisit
+tfs = function (verifiedPaths, command) { // verfied meaning the path and file exisit
 	var log = '';
 	verifiedPaths.forEach(function (filepath) {
 		var commandLine = "tf.exe " + command + " " + filepath;
 		log += shell.exe(commandLine);
 	});
-	callback();
+	if (callback) {
+		callback();
+	}
 	return log;
+},
+workingDirectory;
+
+exports.init = function (param) {
+	callback = param.callback;
+	workingDirectory = param.visualStudioPath;
 };
 
-exports.checkout = function (verifiedPaths, callback) {
-	return tfs(verifiedPaths, 'checkout', callback);
+// TFS command-line http://msdn.microsoft.com/en-us/library/z51z7zy0(v=vs.100).aspx
+exports.checkout = function (verifiedPaths) {
+	return tfs(verifiedPaths, 'checkout');
+};
+exports.undo = function (verifiedPaths) {
+	return tfs(verifiedPaths, 'undo /noprompt');
 };
 
-exports.undo = function (verifiedPaths, callback) {
-	return tfs(verifiedPaths, 'undo /noprompt', callback);
+// Enumeration for visualStudioPath
+exports.vs2008 = {
+	"bit32": 'C:/Program Files/Microsoft Visual Studio 9.0/Common7/IDE/',
+	"bit64": 'C:/Program Files (x86)/Microsoft Visual Studio 9.0/Common7/IDE/'
+};
+exports.vs2010 = {
+	"bit32": 'C:/Program Files/Microsoft Visual Studio 10.0/Common7/IDE/',
+	"bit64": 'C:/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/IDE/'
+};
+exports.vs2012 = {
+	"bit32": 'C:/Program Files/Microsoft Visual Studio 11.0/Common7/IDE/',
+	"bit64": 'C:/Program Files (x86)/Microsoft Visual Studio 11.0/Common7/IDE/'
 };
