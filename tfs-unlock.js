1165a80411aea0c0814595ca11a944e6ceef385f
mgardner <mgardner@sumtotalsystems.com>
2015-01-29 15:06:09 -0500

    removed module: sleep. Converted to use promises. A single callback is used to determine when all paths have been processed.

--- tfs-unlock.js
+++ tfs-unlock.js
@@ -1,116 +1,142 @@
 /*
-* tfs-unlock
-* https://github.com/danactive/tfs-unlock
-*
-* Copyright (c) 2013 Dan BROOKS
-* Licensed under the MIT license.
-*/
+ * tfs-unlock
+ * https://github.com/danactive/tfs-unlock
+ *
+ * Copyright (c) 2013 Dan BROOKS
+ * Licensed under the MIT license.
+ */
 
 'use strict';
 
 var fs = require('fs'),
-shellCallback,
-wait = 3, // sec default delay for TFS to apply commands
-workingDirectory,
-messages = {
-	"shell": {
-		"beginCommand": 'Begin shell command',
-		"noCommand": 'Missing shell command',
-		"stderr": 'Interpreter Error: ',
-		"stdout": 'Interpreter Output: ',
-		"exitCode": 'Child process exited with code # '
-	}
-},
-shell = {
-	"exe": function (command, calleeCallback) {
-		var process;
-		if (command) {
-			process = require('child_process').exec(command, { "cwd": workingDirectory });
-
-			process.stdout.on('data', function (message) {
-				if (calleeCallback) {
-					calleeCallback('stdout', messages.shell.stdout + message + '.');
-				}
-			});
-			process.stderr.on('data', function (message) {
-				if (calleeCallback) {
-					calleeCallback('stderr', messages.shell.stderr + message + '.');
-				}
-			});
-			process.on('close', function (code, signal) {
-				var out = '';
-				out += messages.shell.exitCode + (code || '') + '.';
-				if (signal != null) {
-					out += 'Child process terminated due to receipt of signal ' + signal + '.';
-				}
-				if (calleeCallback) {
-					calleeCallback('close', out);
-				}
-			});
-
-			return messages.shell.beginCommand;
-		} else {
-			return messages.shell.noCommand;
+	Q = require('Q'),
+	shellCallback,
+	wait = 3, // sec default delay for TFS to apply commands
+	workingDirectory,
+	messages = {
+		"shell": {
+			"beginCommand": 'Begin shell command',
+			"noCommand": 'Missing shell command',
+			"stderr": 'Interpreter Error: ',
+			"stdout": 'Interpreter Output: ',
+			"exitCode": 'Child process exited with code # '
 		}
-	}
-},
-tfs = function (paths, command) { // verfied meaning the path and file exisit
-	var sleeper = require('sleep'),
-		commandLine,
-		exists,
-		log = '';
-	if( Object.prototype.toString.call( paths ) !== '[object Array]' ) {
-		throw new TypeError("paths parameter must be an array");
-	}
-	paths.forEach(function (filepath) {
-		filepath = fs.realpathSync(filepath); // resolve full path
-		commandLine = "tf.exe " + command + " " + filepath;
-		exists = fs.existsSync(filepath);
-		if (exists) {
-			shell.exe(commandLine);
-			log += "\n" + filepath;
+	},
+	shell = {
+		"exe": function (command, calleeCallback) {
+			var process,
+				deferred = Q.defer();
+
+			if (command) {
+				process = require('child_process').exec(command, {
+					"cwd": workingDirectory
+				});
+
+				process.stdout.on('data', function (message) {
+					deferred.notify({
+						stream: 'stdout',
+						message: messages.shell.stdout + message + '.'
+					});
+
+				});
+				process.stderr.on('data', function (message) {
+					deferred.notify({
+						stream: 'stderr',
+						message: messages.shell.stderr + message + '.'
+					});
+				});
+				process.on('close', function (code, signal) {
+					var out = '';
+					out += messages.shell.exitCode + (code || '') + '.';
+					if (signal != null) {
+						out += 'Child process terminated due to receipt of signal ' + signal + '.';
+					}
+
+					deferred.resolve(out);
+				});
+
+				deferred.notify({
+					message: messages.shell.beginCommand
+				});
+			} else {
+				deferred.reject(messages.shell.noCommand);
+			}
+
+			return deferred.promise;
+		}
+	},
+	tfs = function (paths, command) { // verfied meaning the path and file exisit
+		var commandLine,
+			exists,
+			log = '';
+
+		if (Object.prototype.toString.call(paths) !== '[object Array]') {
+			throw new TypeError("paths parameter must be an array");
+		}
+
+		_handlePaths(paths).done(function (logs) {
+			console.log(logs);
 			if (shellCallback) {
 				shellCallback();
 			}
+		}, function (err) {
+			console.log('error', err);
+		});
+	},
+	_handlePaths = function (paths) {
+		return Q.all(paths.map(_handlePath));
+	},
+	_handlePath = function (filepath) {
+		var deferred = Q.defer();
+		var log = '';
+		var filepath = fs.realpathSync(filepath); // resolve full path
+		var commandLine = "tf.exe " + command + " " + filepath;
+		var exists = fs.existsSync(filepath);
+
+		if (exists) {
+			shell.exe(commandLine).then(function (out) {
+				log += out;
+				deferred.resolve(log);
+			}, function (err) {
+				deferred.reject(err);
+			}, function (progress) {
+				log += progress;
+			});
+
 		} else {
 			throw new ReferenceError('File path is not found: ' + filepath);
 		}
-	});
 
-	if (wait && wait > 0) {
-		sleeper.sleep(wait);
-	}
-
-	return log;
-},
-findVisualStudioPath = function () {
-	var wd;
-	for (var ver in paths) {
-		if (paths.hasOwnProperty(ver)) {
-			for (var path in paths[ver]) {
-				if (paths[ver].hasOwnProperty(path)) {
-					if (fs.existsSync(paths[ver][path])) {
-						wd = paths[ver][path];
+		return deferred.promise;
+	},
+	findVisualStudioPath = function () {
+		var wd;
+		for (var ver in paths) {
+			if (paths.hasOwnProperty(ver)) {
+				for (var path in paths[ver]) {
+					if (paths[ver].hasOwnProperty(path)) {
+						if (fs.existsSync(paths[ver][path])) {
+							wd = paths[ver][path];
+						}
 					}
 				}
 			}
 		}
-	}
-	return wd;
-};
+		return wd;
+	};
 
 exports.init = function (param) {
 	param = param || {
-		callback: function () { }
+		callback: function () {}
 	};
-	
+
 	shellCallback = param.callback;
 	workingDirectory = param.visualStudioPath;
 	if (!workingDirectory || !fs.existsSync(workingDirectory)) {
 		workingDirectory = findVisualStudioPath();
 	}
 	wait = param.wait || wait;
-	
+
 };
 
 // TFS `mmand-line http://msdn.microsoft.com/en-us/library/z51z7zy0(v=vs.100).aspx
